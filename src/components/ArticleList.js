374f3226fcf79c2af21454f6ad29e66fa74abe76
aquaform <aquaform@yandex.ru>
2017-10-10 00:26:28 +0500

    add akardeon

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Article from './Article';
 
-export default function ArticleList({ articles }) {
-  const articlesElements = articles.map((article) => {
-    return (
-      <li key = { article.id }>
-        <Article article = { article }/>
-      </li>
-    );
-  });
+export default class ArticleList extends Component {
 
-  return (
-    <ul>
-      {  articlesElements }
-    </ul>
-  );
+  state = {
+    openArticleId: null,
+  }
+
+  render() {
+    const articlesElements = this.props.articles.map((article) => {
+      return (
+        <li key = { article.id }>
+          <Article
+            article = { article }
+            isOpen = { article.id === this.state.openArticleId }
+            toggleOpen = { this.toggleOpenArticle.bind(this, article.id) }
+          />
+        </li>
+      );
+    });
+
+    return (
+      <ul>
+        { articlesElements }
+      </ul>
+    );
+  }
+
+  toggleOpenArticle(openArticleId) {
+    this.setState({ openArticleId: openArticleId ? openArticleId : null });
+  }
+
 }
